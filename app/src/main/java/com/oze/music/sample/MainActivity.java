1b425a8cbb76ed3516ecdc6f4e30e26816b5a230
emad <emadabdalrahman11@gmail.com>
2018-12-12 14:48:08 +0200

    update simple to work with last version

--- app/src/main/java/com/oze/music/sample/MainActivity.java
+++ app/src/main/java/com/oze/music/sample/MainActivity.java
@@ -1,7 +1,6 @@
 package com.oze.music.sample;
 
 import android.animation.ValueAnimator;
-import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,25 +13,17 @@ import com.oze.music.musicbar.BigMusicBar;
 import com.oze.music.musicbar.MiniMusicBar;
 import com.oze.music.musicbar.MusicBar;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity implements ValueAnimator.AnimatorUpdateListener {
 
     public static final String TAG = "MainActivity";
     BigMusicBar musicBar;
     MiniMusicBar miniMusicBar;
-    Button next;
-    Button previous;
-    ArrayList<Integer> music = new ArrayList<>();
+    Button hide;
+    Button show;
     MediaPlayer mediaPlayer;
-    int i;
     boolean mSeekBarIsTracking = false;
+    ValueAnimator mValueAnimator;
+
     MusicBar.OnMusicBarAnimationChangeListener onMusicBarAnimationChangeListener = new MusicBar.OnMusicBarAnimationChangeListener() {
         @Override
         public void onHideAnimationStart() {
@@ -55,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements ValueAnimator.Ani
             Log.i(TAG, "onShowAnimationEnd");
         }
     };
+
     MusicBar.OnMusicBarProgressChangeListener onMusicBarProgressChangeListener = new MusicBar.OnMusicBarProgressChangeListener() {
         @Override
         public void onProgressChanged(MusicBar musicBar, int progress, boolean fromUser) {
@@ -82,41 +74,16 @@ public class MainActivity extends AppCompatActivity implements ValueAnimator.Ani
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         musicBar = findViewById(R.id.BigMusicBar);
         miniMusicBar = findViewById(R.id.MiniMusicBar);
 
-        next = findViewById(R.id.next);
-        previous = findViewById(R.id.previous);
-
-        music.add(R.raw.music);
         mediaPlayer = MediaPlayer.create(this, R.raw.music);
         mediaPlayer.start();
 
+        musicBar.loadFrom(getResources().openRawResource(R.raw.music), mediaPlayer.getDuration());
+        miniMusicBar.loadFrom(getResources().openRawResource(R.raw.music),mediaPlayer.getDuration());
+
         initValueAnimator(1.0f, 0, mediaPlayer.getDuration());
-
-        File file = new File(getCacheDir(), "music.mp3");
-        byte[] buffer = null;
-        if (i < music.size()) {
-            buffer = getBytes(file, music.get(0));
-            i++;
-        } else {
-            i = 0;
-            buffer = getBytes(file, music.get(0));
-            i++;
-        }
-
-        MediaMetadataRetriever metadataRetriever = new MediaMetadataRetriever();
-        metadataRetriever.setDataSource(file.getPath());
-
-        int duration = Integer.parseInt(metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)) / 1000;
-
-
-        if (buffer != null){
-            musicBar.loadFrom(buffer, duration);
-            miniMusicBar.loadFrom(buffer,duration);
-        }
-
 
         musicBar.setAnimationChangeListener(onMusicBarAnimationChangeListener);
         musicBar.setProgressChangeListener(onMusicBarProgressChangeListener);
@@ -124,25 +91,25 @@ public class MainActivity extends AppCompatActivity implements ValueAnimator.Ani
         miniMusicBar.setAnimationChangeListener(onMusicBarAnimationChangeListener);
         miniMusicBar.setProgressChangeListener(onMusicBarProgressChangeListener);
 
-        next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onNextClick();
-            }
-        });
-        next.setText("Hide");
-
-        previous.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onPreviousClick();
-            }
-        });
-        previous.setText("Show");
-
+        initButton();
     }
 
-    ValueAnimator mValueAnimator;
+    void initButton() {
+        hide = findViewById(R.id.hide);
+        show = findViewById(R.id.show);
+        hide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                oHideClick();
+            }
+        });
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onShowClick();
+            }
+        });
+    }
 
     private void initValueAnimator(float playbackSpeed, int progress, int max) {
         int timeToEnd = (int) ((max - progress) / playbackSpeed);
@@ -154,32 +121,14 @@ public class MainActivity extends AppCompatActivity implements ValueAnimator.Ani
         }
     }
 
-    private void onNextClick() {
+    private void oHideClick() {
         musicBar.hide();
         miniMusicBar.hide();
     }
 
-    private void onPreviousClick() {
+    private void onShowClick() {
         musicBar.show();
         miniMusicBar.show();
-    }
-
-    private byte[] getBytes(File file, int id) {
-        byte[] buffer = null;
-        try {
-            InputStream stream = getResources().openRawResource(id);
-            OutputStream output = new FileOutputStream(file);
-            buffer = new byte[stream.available()];
-            stream.read(buffer);
-            output.write(buffer);
-            stream.close();
-            output.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return buffer;
     }
 
     @Override
